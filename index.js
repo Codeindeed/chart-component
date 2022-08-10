6dfd5be83a2c6eeedc109786d5fbba7f0323455d
Codeindeed <favouralex084@gmail.com>
2022-08-10 12:14:47 +0200

    optimized code abit

--- index.js
+++ index.js
@@ -20,38 +20,30 @@ function toolTip(graphs) {
   });
 }
 function createGraph(data) {
-  const total = [];
-  data.map((item) => {
-    total.push(item.amount);
-  });
-  let highest = total.reduce((acc, mov) => {
-    if (acc > mov) return acc;
-    else {
-      return mov;
+  let highest = data.reduce(
+    (acc, mov) => (acc.amount > mov.amount ? acc : mov),
+    0
+  );
+  data.forEach((data) => {
+    let body1 = document.createElement("div");
+    body1.classList.add("body1");
+    let graph = document.createElement("div");
+    graph.classList.add("graph");
+    graph.style.height = `${data.amount}%`;
+    if (data.amount === highest.amount) {
+      graph.style.backgroundColor = "hsl(186, 34%, 60%)";
     }
+    let span = document.createElement("span");
+    span.classList.add("tolltip");
+    span.textContent = `$${data.amount}`;
+    let text = document.createElement("p");
+    text.classList.add("text");
+    text.textContent = data.day.toUpperCase();
+    body1.append(graph, span, text);
+    container.appendChild(body1);
   });
-  if (data !== " ") {
-    data.forEach((data, i) => {
-      let body1 = document.createElement("div");
-      body1.classList.add("body1");
-      let graph = document.createElement("div");
-      graph.classList.add("graph");
-      graph.style.height = `${data.amount}%`;
-      if (data.amount === highest) {
-        graph.style.backgroundColor = "hsl(186, 34%, 60%)";
-      }
-      let span = document.createElement("span");
-      span.classList.add("tolltip");
-      span.textContent = `$${data.amount}`;
-      let text = document.createElement("p");
-      text.classList.add("text");
-      text.textContent = data.day.toUpperCase();
-      body1.append(graph, span, text);
-      container.appendChild(body1);
-    });
-    const graphs = document.querySelectorAll(".graph");
-    toolTip(graphs);
-  }
+  const graphs = document.querySelectorAll(".graph");
+  toolTip(graphs);
 }
 async function datas() {
   let obj = await fetch("./data.json");
